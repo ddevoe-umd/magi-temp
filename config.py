432b092016578299e0652defbedb84c5a726df70
ddevoe-umd <84353435+ddevoe-umd@users.noreply.github.com>
2025-01-06 17:35:41 -0500

    Add files via upload

--- config.py
+++ config.py
@@ -2,8 +2,6 @@
 # Cross-module Python global variables:
 
 well_config = []
-well_cols = 0            # number of well columns
-well_rows = 0            # number of well rows
 roi_upper_left = (0,0)   # cordinates for upper left corner of upper left ROI
 roi_width = 0            # box size
 roi_height = 0 
@@ -15,9 +13,16 @@ card_filename = ""
 
 # target_dict is currently copied from magi.js...need to make this come from file...
 target_dict = {          
-  "MecA": ["#4C4CEB", "solid"],   
-  "FemB": ["#5ED649", "solid"],
-  "Nuc": ["#DD4444", "solid"],
+  "mecA": ["#4C4CEB", "solid"],   
+  "femB": ["#5ED649", "solid"],
+  "ermB": ["#FFC0CB", "solid"],
+  "ermF": ["#33CCFF", "solid"],
+  "ermT": ["#FF8C00", "solid"],
+  "ermX": ["#FFFF00", "solid"],
+  "tetA/C": ["#FF0000 ", "solid"],
+  "sul1": ["#000080", "solid"],
+  "sul2": ["#C0C0C0", "solid"],
+  "nuc": ["#DD4444", "solid"],
   "POS": ["#222222", "dash"],
   "NEG": ["#555555", "dot"]
 };
